e677da2c8c23c7ad0e61798d3dc6894c263ebb49
Alejo Castaño Rojas <alejocas@experimentality.co>
2020-02-25 22:26:07 -0500

    Toque final añadido en generateSolutions.sh

--- generateSolutions.sh
+++ generateSolutions.sh
@@ -7,3 +7,5 @@ gcc -Wall p5.c -o p5.out -lm;
 gcc -Wall p6.c -o p6.out -lm;
 gcc -Wall p7.c -o p7.out -lm;
 gcc -Wall p8.c -o p8.out -lm;
+gcc -Wall p9.c -o p9.out -lm;
+echo "Soluciones compiladas por el GCC, pero desarrolladas por Alejandro Castaño Rojas"
